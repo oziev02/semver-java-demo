a29eac1f9a29366b8d758b05cf03455ae9aedce4
oziev02 <magomed.oziev2002@gmail.com>
2025-10-23 09:51:32 +0300

    fix: clarify message text (patch)

--- App.java
+++ App.java
@@ -1,13 +1,13 @@
 package com.example;
 
 public class App {
-    public static final String VERSION = "1.1.0"; // было 1.0.0 → MINOR
+    public static final String VERSION = "1.1.1"; // PATCH
 
     public static void main(String[] args) {
         System.out.println("Hello, SemVer!");
         System.out.println("Version: " + VERSION);
 
-        // Новая фича: печатаем количество аргументов (без имени класса/файла)
-        System.out.println("Args count: " + args.length);
+        // Фикс: более точное сообщение
+        System.out.println("Number of CLI args: " + args.length);
     }
 }
